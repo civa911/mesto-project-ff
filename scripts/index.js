68bcec43d0b6cd923807f5f7374b2210f4b00ce2
Vlad Tsiv <vlad_tsiv82@mail.ru>
2024-10-31 01:27:58 +0300

    add popup and likrcard

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,61 @@
 // @todo: Темплейт карточки
 const cardTemplate = document.querySelector('#card-template').content;
+const imagePopup = document.querySelector('.popup_type_image');
+const popupImage = imagePopup.querySelector('.popup__image');
+const popupCaption = imagePopup.querySelector('.popup__caption');
+const imagePopupCloseButton = imagePopup.querySelector('.popup__close');
 
 // @todo: DOM узлы
+
+
+
+//Функция лайка карточки
+document.addEventListener('DOMContentLoaded', () => {
+    const likeCardButtons = document.querySelectorAll('.card__like-button');
+    
+    likeCardButtons.forEach(button => {
+        button.addEventListener('click', evt => {
+            evt.currentTarget.classList.toggle('card__like-button_is-active');
+        });
+    });
+})
+
+// Функция открытия Popup
+function openImagePopup(imageSrc, imageCaption) {
+  popupImage.src = imageSrc;
+  popupImage.alt = imageCaption;
+  popupCaption.textContent = imageCaption;
+  imagePopup.classList.add('popup_is-opened');
+}
+
+// Функция закрытия Popup
+function closeImagePopup() {
+  imagePopup.classList.remove('popup_is-opened');
+}
+
+// Добавляем обработчик закрытия Popup
+imagePopupCloseButton.addEventListener('click', closeImagePopup);
 
 // @todo: Функция создания карточки
 const createCard = function (cardData) {
     const cardElement = cardTemplate.cloneNode(true);
+    const cardImage = cardElement.querySelector('.card__image');
+    
+    cardImage.src = cardData.link;
+    cardImage.alt = cardData.name;
+    
+    cardImage.addEventListener('click', () => {
+        openImagePopup(cardData.link, cardData.name);
+    });
+    
     cardElement.querySelector('.card__title').textContent = cardData.name;
-    cardElement.querySelector('.card__image').src = cardData.link;
-    cardElement.querySelector('.card__image').alt = cardData.name;
+    
     cardElement.addEventListener('click', addEventListener);
     
     return cardElement;
 }
 
 // @todo: Функция удаления карточки
-
 const deleteCard = function (cardElement) {
     cardElement.remove();
     return cardElement;
